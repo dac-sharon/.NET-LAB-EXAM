73629701f0e26429767bfc2bbbad99d2f8bfcd92
dac-sharon <133682773+dac-sharon@users.noreply.github.com>
2023-07-16 13:19:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LabExam
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Book 1 Information");
+            TestData td = new TestData("BASIC","Kumar Mehra");
+            Console.WriteLine(" Title = "+td.Title+", Author = "+td.Author);
+            Console.WriteLine("Book 2 Information");
+            TestData t = new TestData("C+", "Sainath Sharma");
+            Console.WriteLine(" Title = " + t.Title + ", Author = " + t.Author);
+            Console.WriteLine("Above is the Book Information");
+        }
+    }
+    public class TestData
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public TestData(string title, string author)
+        {
+            this.Title = title; 
+            this.Author = author;
+        }
+    }
+    
+}
